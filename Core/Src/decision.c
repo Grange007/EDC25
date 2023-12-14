db418fe85a66de413bc83230f4e37174607dd4c2
ONLOX <ONLOX@example.com>
2023-12-14 18:33:34 +0800

    Change decision structure

--- Core/Src/decision.c
+++ Core/Src/decision.c
@@ -1,47 +1,79 @@
 #include "decision.h"
+#include "jy62.h"
 #include "main.h"
 #include <math.h>
-#define DIAMOND 1
+
 #define MAX_POS_ERROR 0.1
+
+Status status = init;
+
 uint8_t minWool=8;
 uint8_t bedMinHeight=16;
 uint8_t emeraldCtrlLine=32;
-status stat;
-uint8_t gameMap[64];
-Grid goalGrid={0,0},nowGrid={0,0},homeGrid={0,0};
-Position_edc25 now = {0, 0};
-Position_edc25 goal = {5, 5};
+MapType gameMap[64]
+				= {1, 0, 0, 0, 0, 0, 0, 3,
+				   0, 0, 0, 0, 0, 0, 0, 0,
+				   0, 0, 2, 0, 0, 0, 0, 0,
+				   0, 0, 0, 0, 1, 0, 0, 0,
+				   0, 0, 0, 1, 0, 0, 0, 0,
+				   0, 0, 0, 0, 0, 2, 0, 0,
+				   0, 0, 0, 0, 0, 0, 0, 0,
+				   3, 0, 0, 0, 0, 0, 0, 1};
+
+Grid goalGrid = {0, 0};
+Grid nowGrid = {0, 0};
+Grid homeGrid = {0, 0};
+Grid opHomeGrid = {0, 0};
+Grid desGrid = {0, 0};
+Grid opGrid = {0, 0};
 Grid nearestDiamond;
-uint8_t mhtDst(Grid from, Grid to){
+
+Position_edc25 now = {0, 0};
+Position_edc25 goal = {0, 0};
+Position_edc25 home = {0, 0};
+Position_edc25 opHome = {0, 0};
+Position_edc25 des = {0, 0};
+Position_edc25 op = {0, 0};
+
+uint8_t mhtDst(Grid from, Grid to)
+{
     return abs(from.x-to.x)+abs(from.y-to.y);
 }
-uint8_t grid2No(Grid grid){
+uint8_t grid2No(Grid grid)
+{
     return grid.x+8*grid.y;
 }
-Grid no2Grid(uint8_t no){
+Grid no2Grid(uint8_t no)
+{
     Grid tmp;
     tmp.x=no%8;
     tmp.y=no/8;
     return tmp;
 }
-Grid pos2Grid(Position_edc25 pos){
+Grid pos2Grid(Position_edc25 pos)
+{
     Grid tmp;
     tmp.x=pos.posx+0.5;
     tmp.y=pos.posy+0.5;
     return tmp;
 }
-Position_edc25 grid2Pos(Grid grid){
+Position_edc25 grid2Pos(Grid grid)
+{
     Position_edc25 tmp;
-    tmp.posx=grid.x;
-    tmp.posy=grid.y;
+    tmp.posx = (float)grid.x + 0.5;
+    tmp.posy = (float)grid.y + 0.5;
     return tmp;
 }
-Grid nearestBlock(uint8_t type){
-    Grid nearest={0,0};
+Grid nearestBlock(MapType type)
+{
+    Grid nearest = {0, 0};
     uint8_t dst=255;
-    for(int i=0;i<64;i++){
-        if(gameMap[i]==type){
-            if(mhtDst(nowGrid,no2Grid(i))<dst){
+    for(int i=0;i<64;i++)
+    {
+        if(gameMap[i] == type)
+        {
+            if(mhtDst(nowGrid,no2Grid(i))<dst)
+            {
                 nearest=no2Grid(i);
                 dst=mhtDst(nowGrid,no2Grid(i));
             }
@@ -49,103 +81,103 @@ Grid nearestBlock(uint8_t type){
     }
     return nearest;
 }
-Grid getNext(Grid from, Grid to){
-    if(from.x<to.x){
-        from.x+=1;
-    }
-    else if(from.x>to.x){
-        from.x-=1;
-    }
-    else if(from.y<to.y){
-        from.y+=1;
-    }
-    else if(from.y>to.y){
-        from.y-=1;
-    }
+Grid getNext(Grid from, Grid to)
+{
+    if(from.x < to.x)
+        from.x += 1;
+    else if(from.x > to.x)
+        from.x -= 1;
+    else if(from.y < to.y)
+        from.y += 1;
+    else if(from.y > to.y)
+        from.y -= 1;
     return from;
 }
-void protect_bed_func(){
-    while(getEmeraldCount()>=2){
-        trade_id(3);
-    }
-    while(getHeightOfId(grid2No(homeGrid))<bedMinHeight&&getWoolCount()>minWool){
-        place_block_id(grid2No(homeGrid));
-    }
-    goalGrid=nearestDiamond;
-    stat=move;    
-}
-void mine_func(){
-    bool gotEnough=false;
-    bool conflict=false;
-    Position_edc25 posop;
-    while(!(gotEnough||conflict)){
-        gotEnough=getEmeraldCount()>emeraldCtrlLine;
-        getPositionOpponent(&posop);
-        conflict=pos2Grid(posop).x==nowGrid.x&&pos2Grid(posop).y==nowGrid.y;
-    }
-    goalGrid=homeGrid;
-    stat=move;
-}
-void destroy_bed_func(){
 
+void ready_func()
+{
+	goal = now;
+	goalGrid = nowGrid;
+
+	home = now;
+	homeGrid = nowGrid;
+
+	if (homeGrid.x == 0)
+	{
+		opHomeGrid.x = 7;
+		opHomeGrid.y = 7;
+	}
+	else
+	{
+		opHomeGrid.x = 0;
+		opHomeGrid.y = 0;
+	}
+	opHome = grid2Pos(opHomeGrid);
+	// upd map
 }
-void move_func(){
-    Grid nextGrid=getNext(nowGrid,goalGrid);
-    if(nextGrid.x==nowGrid.x&&nextGrid.y==nowGrid.y){
-        if(nowGrid.x==nearestDiamond.x&&nowGrid.y==nearestDiamond.y){
-            stat=mine;
-            return;
-        }
-        else if(nowGrid.x==homeGrid.x&&nowGrid.y==homeGrid.y){
-            stat=protect;
-            return;
-        }
-    }
-    if(getHeightOfId(grid2No(nextGrid))==0){
-        place_block_id(grid2No(nextGrid));
-    }
-    goal=grid2Pos(nextGrid);
-    Position_edc25 actualPos;
-    do{
-        getPosition(&actualPos);
-    }
-    while((fabs(actualPos.posx-nextGrid.x)+fabs(actualPos.posy-nextGrid.y))>MAX_POS_ERROR);
-}
-void init_func(){
-    nearestDiamond=nearestBlock(DIAMOND);
-    uint8_t diamondDst=mhtDst(nowGrid,nearestDiamond);
-    for(uint8_t i=0;i<8-diamondDst;i++){
+void init_func()
+{
+	InitAngle();
+    nearestDiamond = nearestBlock(diamond);
+    uint8_t diamondDst = mhtDst(nowGrid, nearestDiamond);
+    for(uint8_t i = 0; i < 8 - diamondDst; i++)
+    {
         place_block_id(grid2No(nowGrid));
     }
-    goalGrid=nearestDiamond;
-    stat=move;
+    desGrid = nearestDiamond;
+    status = move;
 }
-void attack_func(){
+void move_func()
+{
+	goalGrid = getNext(nowGrid, desGrid);
+	if(goalGrid.x == nowGrid.x && goalGrid.y == nowGrid.y)
+	{
+		if(nowGrid.x == nearestDiamond.x && nowGrid.y == nearestDiamond.y)
+		{
+			status = mine;
+			return;
+		}
+		else if(nowGrid.x == homeGrid.x && nowGrid.y == homeGrid.y)
+		{
+			status = protect;
+			return;
+		}
+	}
+	if(getHeightOfId(grid2No(goalGrid)) == 0)
+	{
+		place_block_id(grid2No(goalGrid));
+	}
+	goal = grid2Pos(goalGrid);
+}
+void protect_bed_func()
+{
+    if (getEmeraldCount() >= 2)
+    {
+        trade_id(3);
+    }
+    if (getHeightOfId(grid2No(homeGrid)) < bedMinHeight && getWoolCount() > minWool)
+    {
+        place_block_id(grid2No(homeGrid));
+    }
+    else
+    {
+    	desGrid = nearestDiamond;
+    	status = move;
+    }
+}
+void mine_func()
+{
+    if (getEmeraldCount() > emeraldCtrlLine || (opGrid.x == nowGrid.x && opGrid.y == nowGrid.y))
+	{
+    	desGrid = homeGrid;
+		status = move;
+	}
+}
+void destroy_bed_func()
+{
 
 }
-void mainLoop(){
-    while(1){
-        getPosition(&now);
-        nowGrid=pos2Grid(now);
-        switch(stat){
-        case init:
-            init_func();
-            break;
-        case move:
-            move_func();
-            break;
-        case mine:
-            mine_func();
-            break;
-        case protect:
-            protect_bed_func();
-            break;
-        case destroy:
-            destroy_bed_func();
-            break;
-        case attack:
-            attack_func();
-            break;
-        }
-    };
+void attack_func()
+{
+
 }
