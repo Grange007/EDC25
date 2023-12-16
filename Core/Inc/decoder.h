f5ebb6a7cea1b0b1793a9748797318d6a3e58915
Grange <2634070476@qq.com>
2023-12-16 13:59:34 +0800

    ✨ stm32 and k210 communication succeed

--- Core/Inc/decoder.h
+++ Core/Inc/decoder.h
@@ -1,8 +1,8 @@
 ﻿#ifndef DECODER_H
 #define DECODER_H
-
-extern int *map_ptr;
-void decode_init(int *map); // the map to be changed
+#include "main.h"
+extern uint8_t *map_ptr;
+void decode_init(uint8_t *map); // the map to be changed
 void decode_update(char *str); // decode the message and update the given map
 
 #endif
--- Core/Src/visual.c
+++ Core/Src/visual.c
@@ -5,28 +5,30 @@
 #include "main.h"
 #include "usart.h"
 char visual_ch;
-char visual_buffer[10];
+char visual_buffer[500];
 void visual_receive(void)
 {
     HAL_Delay(1000);
-    for (int i = 0; i < 200; i++)
+//    for (int i = 0; i < 200; i++)
     {
-        HAL_UART_Receive(&huart5, &visual_ch, 1, HAL_MAX_DELAY);
-        u1_printf("%c", visual_ch);
-        if (visual_ch == '\n')
-        {
-            decode_update(visual_buffer);
-            memset(visual_buffer, 0, sizeof(visual_buffer));
-            for (int i = 0; i < 64; i++)
-            {
-            	u1_printf("%d ", gameMap[i]);
-            	if ((i + 1) % 8 == 0)
-            		u1_printf("\n");
-            }
-        }
-        else
-        {
-            strncat(visual_buffer, &visual_ch, 1);
-        }
+        HAL_UART_Receive(&huart5, visual_buffer, 500, HAL_MAX_DELAY);
+        visual_buffer[500] = '\0';
+//        u1_printf("%s", visual_buffer);
+        decode_update(visual_buffer);
+//        if (visual_ch == '\n')
+//        {
+//            decode_update(visual_buffer);
+//            memset(visual_buffer, 0, sizeof(visual_buffer));
+//            for (int i = 0; i < 64; i++)
+//            {
+//            	u1_printf("%d ", gameMap[i]);
+//            	if ((i + 1) % 8 == 0)
+//            		u1_printf("\n");
+//            }
+//        }
+//        else
+//        {
+//            strncat(visual_buffer, &visual_ch, 1);
+//        }
     }
 }
